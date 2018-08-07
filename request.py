c49d24419cbb73adcf4be013d18bd3d0ee85ebcd
matheusgmaia <matheus.maia@ccc.ufcg.edu.br>
2018-08-06 21:44:39 -0300

    init

---
+++ request.py
@@ -0,0 +1,30 @@
+# USAGE
+# python simple_request.py
+
+# import the necessary packages
+import requests
+
+# initialize the Keras REST API endpoint URL along with the input
+# image path
+KERAS_REST_API_URL = "https://www.floydlabs.com/serve/goatJyoxkVWSJBGssLaz3i"
+IMAGE_PATH = "leite2.jpeg"
+
+# load the input image and construct the payload for the request
+image = open(IMAGE_PATH, "rb").read()
+payload = {"image": image}
+
+# submit the request
+r = requests.post(KERAS_REST_API_URL, files=payload)
+print(r)
+r = r.json()
+
+# ensure the request was sucessful
+if r["success"]:
+	# loop over the predictions and display them
+	for (i, result) in enumerate(r["predictions"]):
+		print("{}. {}: {:.4f}".format(i + 1, result["label"],
+			result["probability"]))
+
+# otherwise, the request failed
+else:
+	print("Request failed")
